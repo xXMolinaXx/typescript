3b10d8ccc540bfa37dcb33231e27df4d3b981e95
Kenny Molina <kjmolina@live.com>
2022-11-14 19:16:20 -0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,11 @@ app.get('/', function (req, res) {
 app.get('*', function(req, res){
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use(cors());
 app.use('/users',user);
