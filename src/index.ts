66ebefcd42ded72f4cafcbd1e80a7167e8187a20
Kenny Molina <kjmolina@live.com>
2022-11-28 15:51:50 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,19 @@ import { DATABASE_URL } from "./config/server.conf";
 const db = new databaseConnection('base de datos chat honduras',DATABASE_URL);
 db.conectDataBase();
 db.nameDataBase = 'Base de datos'
-io.on('connection', (socket) => {
+io.on('connection',socket => {
+    console.log('Clients conected => ',io.engine.clientsCount);
+    console.log('cocket id => ', socket.id );
 // @ts-ignore
     socket.on("hi",(arg)=>{
         // console.log(arg);
         // socket.broadcast.emit('message','hola desde el otro cliente')
     })
+    socket.on("disconnect",()=>{
+        console.log('cliente desconectado')
+    }) 
 });
+
 server.listen(PORT, function (this:any) {
     console.log('http://localhost:'+this.address().port);
 });
