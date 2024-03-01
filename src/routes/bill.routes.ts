e08595ccc1ffd7211f05b67c255fdf00a322659d
Kenny Molina <kjmolina@live.com>
2024-03-01 13:42:07 -0600

    create bill

--- src/routes/bill.routes.ts
+++ src/routes/bill.routes.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import { checkRoles } from '../middleware/auth.handler'
 import billService from '../services/bill.service'
 import passport from 'passport'
+import { type IBill } from '../common/interface/bill.interface'
 const router = express.Router()
 
 router.post(
@@ -12,11 +13,15 @@ router.post(
   checkRoles('admin', 'client'),
   async (req, res) => {
     try {
-      await billService.create()
+      const newDocument = req.body as IBill
+      await billService.create(newDocument)
       res.json({ sucess: true, message: 'Tu compra a sido realizada' })
     } catch (error: any) {
+      console.log(error)
       res.json({ sucess: false, message: 'Error al generar tu compra' })
     }
     res.end()
   }
 )
+
+export default router
--- src/services/bill.service.ts
+++ src/services/bill.service.ts
@@ -1,8 +1,15 @@
 import { type IBill } from '../common/interface/bill.interface'
 import BillSchema from '../models/bill.schema'
+import ProductSchema from '../models/product.schema'
 class Bill {
   async create (dataBill: IBill): Promise<void> {
-    await new BillSchema(dataBill).save()
+    let totalPay = 0
+    const products = await ProductSchema.find({ _id: { $in: dataBill.products } }).exec()
+    products.forEach(product => { totalPay += product.price })
+    await new BillSchema({
+      totalPay,
+      products: dataBill.products
+    }).save()
   }
 }
 const billObject = new Bill()
