8e5b3a37444d5684749ac90b75110cf73589e1e6
Kenny Molina <42689547+xXMolinaXx@users.noreply.github.com>
2024-06-12 09:03:36 -0600

    Update server.conf.ts

--- src/common/config/server.conf.ts
+++ src/common/config/server.conf.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const CONFIGURATION = {
   IS_QA: process.env.IS_QA || false,
-  PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost:27017/chat_hn",
   JWT_SECRET: process.env.JWT_SECRET || "contrasena",
 };
